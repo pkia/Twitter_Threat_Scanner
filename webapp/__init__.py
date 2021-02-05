dafca48f2e8f9bc4762efdeafcd9949f3f041ee8
Conor Heeney <conor.heeney56@gmail.com>
2021-02-05 16:31:11 +0000

    Created initial scan entry page

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,5 +2,9 @@ from flask import Flask
 
 
 app = Flask(__name__)
+#import secrets
+#secrets.token_hex(16)
+app.config['SECRET_KEY'] = '3efb586753c505b1b6dc3672351a27cf'
+
 
 from webapp import views
---
+++ webapp/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class ScanForm(FlaskForm):
+
+    your_handle = StringField("Enter your handle to scan your followers", validators=[DataRequired()])
+    submit_my_handle = SubmitField("Get Followers")
+    user_handle = StringField("or enter a user's handle to see their report", validators=[DataRequired()])
+    submit_other_handle = SubmitField("View Report")
