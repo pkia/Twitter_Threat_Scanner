260eb8b4fa4feb35f85012b2c4569ee749835498
Conor Heeney <conor.heeney56@gmail.com>
2021-02-05 17:26:36 +0000

    Added redirects and errors depending on scan input

--- webapp/forms.py
+++ webapp/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired
 
 class ScanForm(FlaskForm):
 
-    your_handle = StringField("Enter your handle to scan your followers", validators=[DataRequired()])
+    your_handle = StringField("Enter your handle to scan your followers")
     submit_my_handle = SubmitField("Get Followers")
-    user_handle = StringField("or enter a user's handle to see their report", validators=[DataRequired()])
+    user_handle = StringField("or enter a user's handle to see their report")
     submit_other_handle = SubmitField("View Report")
--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,5 @@
 from webapp import app
-from flask import render_template, url_for
+from flask import render_template, url_for, redirect, flash
 from webapp.forms import ScanForm
 
 @app.route("/")
@@ -7,10 +7,20 @@ from webapp.forms import ScanForm
 def index():
 	return render_template('index.html')
 
-@app.route("/scan")
+@app.route("/scan", methods=["GET", "POST"])
 def scan():
 	form = ScanForm()
+	if form.your_handle.data and form.user_handle.data:
+		flash("Only one field required", "danger")
+	elif form.your_handle.data:
+		return redirect(url_for('personal_scan'))
+	elif form.submit_other_handle.data:
+		return redirect(url_for('report'))
 	return render_template('scan.html', title='Scan', form=form)
+
+@app.route("/personal_scan")
+def personal_scan():
+	return render_template('personal_scan.html', title='Personal Scan')
 
 @app.route("/report")
 def report():
