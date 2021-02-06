cf45f0958ec0de4360cbd92b36b1db5794ec2a5e
Conor Heeney <conor.heeney56@gmail.com>
2021-02-06 15:55:47 +0000

    Split forms up to remove need for error checks

--- webapp/forms.py
+++ webapp/forms.py
@@ -2,9 +2,12 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
-class ScanForm(FlaskForm):
+class ScanSelfForm(FlaskForm):
 
-    your_handle = StringField("Enter your handle to scan your followers")
-    submit_my_handle = SubmitField("Get Followers")
-    user_handle = StringField("or enter a user's handle to see their report")
-    submit_other_handle = SubmitField("View Report")
+    handle = StringField("Enter your handle to scan your followers", validators=[DataRequired()])
+    submit = SubmitField("Get Followers")
+
+class ScanOtherForm(FlaskForm):
+
+    handle = StringField("or enter a user's handle to see their report", validators=[DataRequired()])
+    submit = SubmitField("View Report")
