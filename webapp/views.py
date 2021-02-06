025b799f978050b989c0003355c7e4b80eddcb64
conormcgavin <72816810+conormcgavin@users.noreply.github.com>
2021-02-06 13:30:07 +0000

    small bug fixes and ordered totals page correctly
    if scan / reports returns has no results yet then message shown saying no results and option to scan/report given
    scanning a user with no followers now works
    fetches all followers now rather than first 15, but extremely slow if account has too many followers on scan all func

--- webapp/views.py
+++ webapp/views.py
@@ -3,7 +3,7 @@ from webapp.form import ReportForm, SearchForm, ScanForm1, ScanForm2
 from webapp.models import Account, Report, ScanResult
 from webapp import app
 from webapp import db
-from sqlalchemy import func
+from sqlalchemy import func, desc
 from webapp import api
 from datetime import datetime, timedelta
 from webapp import tweepy
@@ -21,33 +21,37 @@ def scan():
     scan_user_form = ScanForm2()
     if scan_user_form.submit2.data and scan_user_form.validate_on_submit():
         try:
-            api.get_user(scan_user_form.username.data)
-            return redirect(url_for("scan_user", username=scan_user_form.username.data))
+            acc = api.get_user(scan_user_form.username.data)
+            if acc.protected:
+                flash(f"Sorry. That account is protected", "Failure")
+            else:
+                return redirect(url_for("scan_user", username=scan_user_form.username.data))
         except:
              flash(f"Sorry. That account does not exist", "Failure")
 
     if scan_all_form.submit1.data and scan_all_form.validate_on_submit():
         try:
-            api.get_user(scan_all_form.username.data)
-            return redirect(url_for("scan_all", username=scan_all_form.username.data))
+            acc = api.get_user(scan_user_form.username.data)
+            if acc.protected:
+                flash(f"Sorry. That account is protected", "Failure")
+            else:
+                return redirect(url_for("scan_all", username=scan_all_form.username.data))
         except:
              flash(f"Sorry. That account does not exist", "Failure")
              
-    return render_template('scan_home.html', scan_all_form=scan_all_form, scan_user_form=scan_user_form, title="Scan Home")
+    return render_template('scan_home.html', scan_all_form=scan_all_form, scan_user_form=scan_user_form, title="Scan")
 
 
 @app.route("/scan/user/<string:username>")
 def scan_user(username):
-    
     result = scan_user_function(username)
     user_profile = get_twitter_info(username)
+    
     if result != None:
         account = Account.query.filter_by(id=username).first()
-        clearScan = False
     else:
         account = None
-        clearScan = True
-    return render_template('scan_user.html', result=result, user_profile=user_profile, account=account, clearScan=clearScan, title="Scan User")
+    return render_template('scan_user.html', result=result, user_profile=user_profile, account=account, title="Scan A User")
 
 # make scan_all_function return an account rather than a username so i can include reports
 
@@ -57,15 +61,12 @@ def scan_all(username):
     results = scan_all_function(username)
     user_profile = get_twitter_info(username)
     bad_user_profiles=[]
-    clearScan = False
     length = len(results)
     if length > 0:
         for result in results:
             profile = get_twitter_info(result[0])
             bad_user_profiles.append(profile)
-    else:
-        clearScan=True
-    return render_template('scan_all.html', user_profile=user_profile, bad_user_profiles=bad_user_profiles, length=length, results=results, clearScan=clearScan, title="Scan All")
+    return render_template('scan_all.html', user_profile=user_profile, bad_user_profiles=bad_user_profiles, length=length, results=results, title="Scan All")
 
 
 @app.route("/report", methods=["GET", "POST"])
@@ -105,20 +106,24 @@ def database():
 @app.route("/database/<string:username>")
 def database_search(username):
     page = request.args.get("page", 1, type=int)
-    account = Account.query.filter_by(id=username).first()
+    try:
+        scan_results = ScanResult.query.filter_by(account_id=username).first()
+    except:
+        scan_results = None
     try:
         reports = Report.query.filter_by(account_holder=account).order_by(Report.date_submitted.desc()).paginate(per_page=5)
     except:
         reports = None
     user_profile = get_twitter_info(username)
-    return render_template('database_search.html', reports=reports, account=account, user_profile=user_profile, title="Database Search")
+    return render_template('database_search.html', reports=reports, scan_results=scan_results, user_profile=user_profile, title="Database Search")
 
 
 @app.route("/database/report_ranked")
 def report_ranked():
     page = request.args.get("page", 1, type=int)
     count = db.session.query(Report.account_id, func.count(Report.account_id).label(
-        "total")).group_by(Report.account_id).order_by("total")
+        "total")).group_by(Report.account_id)
+    count = count.order_by(desc('total'))
     counts2 = count.paginate(per_page=5)
     user_profiles = []
     for tup in count:
@@ -138,20 +143,21 @@ def login():
 
 
 def scan(username):
-    if random.randint(0,2) == 1 or 2:
-        result = None
-        return result
+    ran = random.randint(0,5)
+    if ran <= 2:
+        return None
+    if Account.query.filter_by(id=username).first() == None:
+        account = Account(id=username)
+        db.session.add(account)
     result = ScanResult(threat_detected="racist", threat_level=9, account_id=username)
-    account = Account(id=username)
     db.session.add(result)
-    db.session.add(account)
     db.session.commit()
     return result
 
 #TODO: implement something that deletes old scan results if new one given
 
 
-def scan_user_function(username, redo=False):
+def scan_user_function(username, redo=True):
     account = Account.query.filter_by(id=username).first() # searches for account
     if account != None: 
         scan_result = ScanResult.query.filter_by(account_id=username).first() # if present and scanned less than two weeks ago, return that scan
@@ -160,7 +166,7 @@ def scan_user_function(username, redo=False):
             date_two_weeks_ago = date - timedelta(days=14)
         
             if date > date_two_weeks_ago:
-                if redo:
+                if redo == True:
                     scan_result = scan(username)
             else:
                 scan_result = scan(username)
@@ -175,6 +181,8 @@ def scan_all_function(username):
     followers = get_followers(username) # get all a user's followers
     bad = [] # list of usernames where something bad was found
     for follower in followers:
+        if get_twitter_info(follower) == False:
+            continue
         scan_result = scan_user_function(follower) # get scan_results of every follower
         if scan_result != None:
             results = [follower, scan_result, Account.query.filter_by(id=follower)] # 
@@ -188,6 +196,6 @@ def get_twitter_info(screen_name):
 
 def get_followers(username):
     followersList = []
-    for follower in api.followers(username, include_user_entities=False):
-        followersList.append(follower.screen_name)
+    for follower in api.followers_ids(username, include_user_entities=False):
+        followersList.append(follower)
     return followersList
