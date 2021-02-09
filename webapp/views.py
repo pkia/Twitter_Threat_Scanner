360a430c04f06db87711ee2fefd4411840130e47
conormcgavin <72816810+conormcgavin@users.noreply.github.com>
2021-02-09 16:30:28 +0000

    added functionality for scan_choose in views.py
    gives user an option of up to 50 followers to choose, rather than all of their followers
    the latter causes lag

--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, flash, redirect, request
-from webapp.forms import ReportForm, SearchForm, ScanForm1, ScanForm2, LoginForm, RegisterForm
+from webapp.forms import ReportForm, SearchForm, ScanForm1, ScanForm2, LoginForm, RegisterForm, SliderForm
 from webapp.models import Account, Report, ScanResult
 from webapp import app
 from webapp import db
@@ -20,6 +20,7 @@ def scan():
     scan_all_form = ScanForm1()
     scan_user_form = ScanForm2()
     if scan_user_form.submit2.data and scan_user_form.validate_on_submit():
+        scan_user_form.username.data = scan_user_form.username.data.lower()
         try:
             acc = api.get_user(scan_user_form.username.data)
             if acc.protected:
@@ -30,12 +31,13 @@ def scan():
              flash(f"Sorry. That account does not exist", "Failure")
 
     if scan_all_form.submit1.data and scan_all_form.validate_on_submit():
+        scan_all_form.username.data = scan_all_form.username.data.lower()
         try:
-            acc = api.get_user(scan_user_form.username.data)
+            acc = api.get_user(scan_all_form.username.data)
             if acc.protected:
                 flash(f"Sorry. That account is protected", "Failure")
             else:
-                return redirect(url_for("scan_all", username=scan_all_form.username.data))
+                return redirect(url_for("scan_choose", username=scan_all_form.username.data))
         except:
              flash(f"Sorry. That account does not exist", "Failure")
              
@@ -49,9 +51,24 @@ def scan_user(username):
     return render_template('scan_user.html', result=result, user_profile=user_profile, title="Scan A User")
 
 
-@app.route("/scan/all/<string:username>")
-def scan_all(username):
-    results = scan_all_function(username)
+@app.route("/scan/choose/<string:username>", methods=["GET", "POST"])
+def scan_choose(username):
+    slider_form = SliderForm()
+    if slider_form.submit.data:
+        print(slider_form.follower_count.data)
+        return redirect(url_for("scan_all", username=username, follower_count=slider_form.follower_count.data))
+    followers = get_followers(username)
+    follower_list = []
+    for follower in followers:
+        follower_profile = get_twitter_info(follower)
+        follower_list.append(follower_profile)
+    return render_template("scan_choose.html", follower_list=follower_list, form=slider_form, title="Choose Followers To Scan")
+    
+    
+@app.route("/scan/all/<string:username>/<int:follower_count>")
+def scan_all(username, follower_count):
+    followers = get_followers(username, follower_count)
+    results = scan_all_function(username, followers)
     user_profile = get_twitter_info(username)
     bad_user_profiles=[]
     length = len(results)
@@ -59,13 +76,14 @@ def scan_all(username):
         for result in results:
             profile = get_twitter_info(result[0])
             bad_user_profiles.append(profile)
-    return render_template('scan_all.html', user_profile=user_profile, bad_user_profiles=bad_user_profiles, length=length, results=results, title="Scan All")
+    return render_template('scan_all.html', user_profile=user_profile, bad_user_profiles=bad_user_profiles, followers=followers, length=length, results=results, title="Scan All")
 
 
 @app.route("/report", methods=["GET", "POST"])
 def report():
     form = ReportForm()
     if form.validate_on_submit():
+        form.username.data = form.username.data.lower()
         try:
             api.get_user(form.username.data)
             report = Report(account_id=form.username.data, threat_type=form.threat_field.data, summary=form.summary.data)
@@ -85,6 +103,7 @@ def report():
 def database():
     form = SearchForm()
     if form.validate_on_submit():
+        form.username.data = form.username.data.lower()
         try:
             api.get_user(form.username.data)
             return redirect(url_for("database_search", username=form.username.data, page=1))
@@ -159,8 +178,7 @@ def scan_user_function(username):
     scan_result = scan(username)
     return scan_result
     
-def scan_all_function(username):
-    followers = get_followers(username) # get all a user's followers
+def scan_all_function(username, followers):
     bad = [] # list of usernames where something bad was found
     for follower in followers:
         profile = get_twitter_info(username)
@@ -169,7 +187,7 @@ def scan_all_function(username):
         	continue
         scan_result = scan_user_function(follower) # get scan_results of every follower
         if scan_result != None:
-            results = [follower, scan_result, Account.query.filter_by(id=follower)] # 
+            results = [follower, scan_result] # 
             bad.append(results)
     return bad
 
@@ -178,12 +196,12 @@ def get_twitter_info(screen_name):
     profile = [user_info.screen_name, user_info.name, user_info.profile_image_url, user_info.protected]
     return profile
 
-def get_followers(screenname, limit=50):
+def get_followers(screenname, limit=10):
     follower_list = []
     if limit < 0:
         raise Exception("Enter a number above 0!")
-    elif limit > 50:  # Basic error control
-        raise Exception("Max limit 50")
+    elif limit > 100:  # Basic error control
+        raise Exception("Max limit 100")
     for follower in tweepy.Cursor(api.followers, screenname).items(int(limit)):  # Uses tweepys cursor function to add most recent followers to a list
         follower_list.append(follower.screen_name)
     return follower_list
