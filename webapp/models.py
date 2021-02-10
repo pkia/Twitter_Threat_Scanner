f2709855ec4b1c2434b000ab989e8c67818a3e78
Mark Daly <118356771@umail.ucc.ie>
2021-02-10 23:12:12 +0000

    add twitter oauth login functionality to application

--- webapp/models.py
+++ webapp/models.py
@@ -1,5 +1,23 @@
 from datetime import datetime
-from webapp import db
+from webapp import db, twitter_blueprint, login_manager
+from flask_login import UserMixin, current_user
+from flask_dance.consumer.storage.sqla import OAuthConsumerMixin, SQLAlchemyStorage
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(250), unique=True)
+
+class OAuth(OAuthConsumerMixin, db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    user = db.relationship(User)
+
+twitter_blueprint.backend = SQLAlchemyStorage(OAuth, db.session, user=current_user)
+
 
 class Report(db.Model):
     id = db.Column(db.Integer, primary_key=True)
