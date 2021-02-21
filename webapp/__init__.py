61fbb5819403ebb2877cbd584b3a609c61f32c63
Mark Daly <118356771@umail.ucc.ie>
2021-02-21 15:39:22 +0000

    update access keys

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -8,17 +8,19 @@ app = Flask(__name__)
 #secrets.token_hex(16)
 app.config["SECRET_KEY"] = '0e1892da231b615d2c3144c49ed3fe69'
 
-'''
-consumer_key = 'GjjvbJew47BO9z7NaML7QfvkR'
-consumer_secret = '6Llv4LplqMvm3vBXycPI64pgEyTzAXPkPNJe0bQHtWqZFpKULr'
-access_key = '1354421821744029700-k2IIBGBQs5T0pJJDUkMoI5qfpBqdSS'
-access_secret = 'Ed7V4uOq7PwBGcoEKccz6lSSu6TnFWm9M0drE7NzL2ueS'
-'''
+#marks keys
+consumer_key = 'yY98pqqvjxSQtua2IjCx0GOhP'
+consumer_secret = 'z2Umqy3CnS484yOjDZ9hw0pT0JEG36p9AfiBhd57xwhGNnT3zF'
+access_key = '1354421821744029700-G4y8X7P2pwLbKIo3KVXHF3iQs1MSb3'
+access_secret = '9MWD5hMyf4UEThXdMFL4wuiwyiX77oxZyGQIakM8nXAtb'
 
+'''
+#evans keys
 consumer_key = 'T9Zf9RjXPzUR74MElwXSc322V'
 consumer_secret = 'EcFc0W8ZMVHVr9zPViClxeinjkNDSaMRH33vC9SRkv4J5CgB58'
 access_key = '2370053247-nuZnBzC316VEGYtCHPDWP47VFh7wAis3blVtoLZ'
 access_secret = 'Ra6uNqSNztob2UmLQG6nVTuFNF608CI3190CkxndNSeik'
+'''
 
 import tweepy
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
