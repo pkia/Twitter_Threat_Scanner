4294e1fa469ccad872735812cd3a4d8b22593f89
ConorH99 <conor.heeney56@gmail.com>
2021-03-01 17:47:11 +0000

    added pagination to scan_user

--- webapp/static/site.js
+++ webapp/static/site.js
@@ -5,3 +5,12 @@ function showLoader() {
     siteIcon.replaceWith(loaderDiv);
     
 }
+
+function modifyDom(num, tweets) {
+    let tweetElements = document.querySelectorAll(".tweet p");
+    let tweetArray = tweets["tweet-" + num.toString()]["array"];
+    let arrayLength = tweets["tweet-" + num.toString()]["length"];
+    for (i=0; i<arrayLength; i++) {
+        tweetElements[i].innerText = tweetArray[i][1][1];
+    }
+}
