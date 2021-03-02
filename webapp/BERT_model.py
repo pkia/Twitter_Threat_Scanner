ff7a0855bf65d11cb243526710ff2cfdfc354c24
EV <43516563+pkia@users.noreply.github.com>
2021-03-02 14:27:52 +0000

    model

---
+++ webapp/BERT_model.py
@@ -0,0 +1,6 @@
+from happytransformer import HappyTextClassification
+from joblib import dump
+
+happy_tc = HappyTextClassification('BERT', 'Hate-speech-CNERG/dehatebert-mono-english', 2)
+model = happy_tc
+dump(model, filename="BERT.joblib")
