4955423f971d51ce0e3d200d136067f0856fed6e
ConorH99 <conor.heeney56@gmail.com>
2021-03-02 14:16:31 +0000

    fixed pagination bug

--- webapp/static/site.js
+++ webapp/static/site.js
@@ -8,9 +8,14 @@ function showLoader() {
 
 function modifyDom(num, tweets) {
     let tweetElements = document.querySelectorAll(".tweet p");
+    let tweetContainers = document.querySelectorAll(".tweet");
+    for (let container of tweetContainers) {
+        container.style.display = "none"
+    }
     let tweetArray = tweets["tweet-" + num.toString()]["array"];
     let arrayLength = tweets["tweet-" + num.toString()]["length"];
     for (i=0; i<arrayLength; i++) {
+        tweetContainers[i].style.display = "block";
         tweetElements[i].innerText = tweetArray[i][1][1];
     }
 }
